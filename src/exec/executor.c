9a500870a7822bf178cd6861729fb57d48591999
seungwoo <seungwoo7050@naver.com>
2025-03-27 16:47:07 +0900

    feat(executor): execute single external commands
    simple external command가 prepared argv와 env store 직렬화 envp를 사용해 fork, execve, wait 흐름으로 동작하도록 executor를 확장했습니다.
    아직 pipeline과 and/or 실행은 미지원이지만 bare command와 직접 경로 외부 명령은 shell status 매핑과 함께 실제 프로세스로 실행됩니다.

--- src/exec/executor.c
+++ src/exec/executor.c
@@ -1,9 +1,139 @@
 #include "shell/exec.h"
 
 #include "shell/prepare.h"
+#include "shell/support/alloc.h"
+#include "shell/support/error.h"
 
 #include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
+
+static void	sh_executor_free_vector(char **values)
+{
+	size_t	index;
+
+	if (values == NULL)
+		return ;
+	index = 0;
+	while (values[index] != NULL)
+	{
+		free(values[index]);
+		index++;
+	}
+	free(values);
+}
+
+static char	**sh_executor_command_argv(const t_simple_command *command)
+{
+	char	**argv;
+	size_t	index;
+
+	argv = sh_xcalloc(command->argc + 1, sizeof(char *));
+	index = 0;
+	while (index < command->argc)
+	{
+		argv[index] = sh_xstrdup(command->argv[index].text);
+		index++;
+	}
+	return (argv);
+}
+
+static void	sh_executor_child_exec(t_shell *shell, char **argv, char **envp)
+{
+	char	*resolved_path;
+	int		exit_status;
+
+	resolved_path = sh_executor_resolve_command_path(shell, argv[0]);
+	if (resolved_path == NULL)
+	{
+		sh_error(argv[0], "command not found");
+		sh_executor_free_vector(envp);
+		sh_executor_free_vector(argv);
+		_exit(127);
+	}
+	execve(resolved_path, argv, envp);
+	exit_status = sh_executor_status_from_system_error(errno);
+	sh_perror(argv[0], NULL);
+	free(resolved_path);
+	sh_executor_free_vector(envp);
+	sh_executor_free_vector(argv);
+	_exit(exit_status);
+}
+
+static int	sh_executor_wait_child(pid_t child_pid)
+{
+	int	wait_status;
+
+	wait_status = 0;
+	if (waitpid(child_pid, &wait_status, 0) < 0)
+		return (sh_executor_status_from_system_error(errno));
+	return (sh_executor_status_from_waitpid(wait_status));
+}
+
+static int	sh_executor_run_external_command(t_shell *shell,
+		t_simple_command *command)
+{
+	pid_t	child_pid;
+	char	**argv;
+	char	**envp;
+	int		status;
+
+	if (command->argc == 0)
+		return (0);
+	argv = sh_executor_command_argv(command);
+	envp = sh_env_store_to_envp(&shell->env);
+	child_pid = fork();
+	if (child_pid < 0)
+	{
+		status = sh_executor_status_from_system_error(errno);
+		sh_perror(command->argv[0].text, "fork");
+		sh_executor_free_vector(envp);
+		sh_executor_free_vector(argv);
+		return (status);
+	}
+	if (child_pid == 0)
+		sh_executor_child_exec(shell, argv, envp);
+	sh_executor_free_vector(envp);
+	sh_executor_free_vector(argv);
+	return (sh_executor_wait_child(child_pid));
+}
+
+static int	sh_executor_run_pipeline(t_shell *shell, t_pipeline *pipeline)
+{
+	if (pipeline->size != 1)
+	{
+		sh_error("pipeline", "not supported yet");
+		return (1);
+	}
+	return (sh_executor_run_external_command(shell, &pipeline->head->command));
+}
+
+static int	sh_executor_run_and_or_list(t_shell *shell, t_and_or_list *list)
+{
+	if (list->size != 1)
+	{
+		sh_error("and/or", "not supported yet");
+		return (1);
+	}
+	return (sh_executor_run_pipeline(shell, &list->head->pipeline));
+}
+
+static int	sh_executor_run_sequence_list(t_shell *shell,
+		t_sequence_list *program)
+{
+	t_sequence_list_node	*node;
+	int						status;
+
+	status = 0;
+	node = program->head;
+	while (node != NULL)
+	{
+		status = sh_executor_run_and_or_list(shell, &node->and_or);
+		node = node->next;
+	}
+	return (status);
+}
 
 int	sh_executor_status_from_waitpid(int wait_status)
 {
@@ -29,6 +159,7 @@ int	sh_executor_status_from_system_error(int error_number)
 int	sh_executor_run(t_shell *shell, t_sequence_list *program)
 {
 	t_shell_signal_phase	previous_phase;
+	int						status;
 
 	previous_phase = shell->signal_phase;
 	shell->signal_phase = SH_SIGNAL_PHASE_EXECUTE;
@@ -42,7 +173,8 @@ int	sh_executor_run(t_shell *shell, t_sequence_list *program)
 		shell->signal_phase = previous_phase;
 		return (shell->last_status);
 	}
+	status = sh_executor_run_sequence_list(shell, program);
 	shell->signal_phase = previous_phase;
-	shell->last_status = 0;
+	shell->last_status = status;
 	return (shell->last_status);
 }
